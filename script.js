150c8fe5c98796c3aec41ed836cc2b2c2088bffa
pemanentomat <118662112+ChintaFitriana@users.noreply.github.com>
2023-04-14 21:16:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let data = [];
+
+function tampil() {
+  let tabel = document.getElementById('tabel');
+  tabel.innerHTML = '<tr><th>No</th><th>Jurusan</th><th>Action</th></tr>';
+  for (let i = 0; i < data.length; i++) {
+    let btnEdit = `<button class="btn-edit"onclick="edit(${i})">Edit</button>`;
+    let btnHapus = `<button class="btn-hapus"onclick="hapus(${i})">Hapus</button>`;
+    j = i + 1;
+    tabel.innerHTML += `<tr><td>${j}</td><td>${data[i]}</td><td>${btnEdit}${btnHapus}</td></tr>`;
+  }
+}
+
+function tambah() {
+  const input = document.querySelector('input[name=jurusan]');
+  data.push(input.value);
+  tampil();
+}
+
+function edit (id) {
+   const baru = prompt('Edit Jurusan', data [id]);
+   data [id] = baru;
+   tampil();
+}
+
+function hapus (id) {
+    data.splice(id, 1);
+    tampil();
+ }
+
+tampil();
